bfc96098e15e1742275f747075f557831d783e3c
Ted Kisner <tskisner.public@gmail.com>
2025-01-22 23:17:40 -0800

    More work on testing workflows

--- wheels/install_deps_osx.sh
+++ wheels/install_deps_osx.sh
@@ -12,6 +12,11 @@ scriptdir=$(pwd)
 popd >/dev/null 2>&1
 echo "Wheel script directory = ${scriptdir}"
 
+# Build in a subdirectory
+depbuild="${scriptdir}/../deps"
+mkdir -p "${depbuild}"
+pushd "${depbuild}"
+
 # Build options.
 
 #use_gcc=yes
@@ -37,9 +42,6 @@ fi
 MAKEJ=2
 
 PREFIX=/usr/local
-
-# Install library dependencies with homebrew
-brew install flac
 
 # Optionally install gcc
 if [ "x${use_gcc}" = "xyes" ]; then
@@ -111,3 +113,45 @@ tar xjf ${boost_pkg} \
     cxxflags="${CXXFLAGS}" ${extra_link} \
     variant=release threading=multi link=shared runtime-link=shared install \
     && popd >/dev/null 2>&1
+
+# Install libFLAC
+
+flac_version=1.4.3
+flac_dir=flac-${flac_version}
+flac_pkg=${flac_dir}.tar.gz
+
+echo "Fetching libFLAC..."
+
+if [ ! -e ${flac_pkg} ]; then
+    curl -SL "https://github.com/xiph/flac/archive/refs/tags/${flac_version}.tar.gz" -o "${flac_pkg}"
+fi
+
+echo "Building libFLAC..."
+
+rm -rf ${flac_dir}
+tar xzf ${flac_pkg} \
+    && pushd ${flac_dir} >/dev/null 2>&1 \
+    && mkdir -p build \
+    && pushd build >/dev/null 2>&1 \
+    && cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_C_COMPILER="${CC}" \
+    -DCMAKE_C_FLAGS="${CFLAGS}" \
+    -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+    -DBUILD_DOCS=OFF \
+    -DWITH_OGG=OFF \
+    -DBUILD_CXXLIBS=OFF \
+    -DBUILD_PROGRAMS=OFF \
+    -DBUILD_UTILS=OFF \
+    -DBUILD_TESTING=OFF \
+    -DBUILD_EXAMPLES=OFF \
+    -DBUILD_SHARED_LIBS=ON \
+    -DINSTALL_MANPAGES=OFF \
+    -DENABLE_MULTITHREADING=ON \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    .. \
+    && make -j ${MAKEJ} install \
+    && popd >/dev/null 2>&1 \
+    && popd >/dev/null 2>&1
+
+popd
