1ca8316b01455658f0f4e3ea0d53d84569fadc5c
Ted Kisner <tskisner.public@gmail.com>
2025-01-23 23:12:22 -0800

    Switch back to clang and try to debug wheel linking.

--- wheels/install_deps_osx.sh
+++ wheels/install_deps_osx.sh
@@ -19,8 +19,8 @@ pushd "${depbuild}"
 
 # Build options.
 
-use_gcc=yes
-#use_gcc=no
+#use_gcc=yes
+use_gcc=no
 gcc_version=14
 
 if [ "x${use_gcc}" = "xyes" ]; then
@@ -78,7 +78,7 @@ pyver=$(python3 --version 2>&1 | awk '{print $2}' | sed -e "s#\(.*\)\.\(.*\)\..*
 # numpy_ver="2.0.1"
 
 # Install build requirements.
-CC="${CC}" CFLAGS="${CFLAGS}" pip install -v "numpy" scipy astropy
+CC="${CC}" CFLAGS="${CFLAGS}" pip install -v numpy scipy astropy
 
 # Install boost
 
