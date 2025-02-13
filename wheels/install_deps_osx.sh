9170ba3c4c385d985f214104fa62517aabfc8cd0
Theodore Kisner <tskisner.public@gmail.com>
2025-02-12 20:39:14 -0800

    Use homebrew flac on macos

--- wheels/install_deps_osx.sh
+++ wheels/install_deps_osx.sh
@@ -47,6 +47,9 @@ PREFIX=/usr/local
 # https://github.com/actions/runner-images/issues/9272
 sudo chown -R runner:admin /usr/local
 
+# Install library dependencies with homebrew
+brew install flac
+
 # Optionally install gcc
 if [ "x${use_gcc}" = "xyes" ]; then
     brew install gcc@${gcc_version}
@@ -101,44 +104,44 @@ tar xjf ${boost_pkg} \
     variant=release threading=multi link=shared runtime-link=shared install \
     && popd >/dev/null 2>&1
 
-# Install libFLAC
+# # Install libFLAC
 
-flac_version=1.4.3
-flac_dir=flac-${flac_version}
-flac_pkg=${flac_dir}.tar.gz
+# flac_version=1.4.3
+# flac_dir=flac-${flac_version}
+# flac_pkg=${flac_dir}.tar.gz
 
-echo "Fetching libFLAC..."
+# echo "Fetching libFLAC..."
 
-if [ ! -e ${flac_pkg} ]; then
-    curl -SL "https://github.com/xiph/flac/archive/refs/tags/${flac_version}.tar.gz" -o "${flac_pkg}"
-fi
+# if [ ! -e ${flac_pkg} ]; then
+#     curl -SL "https://github.com/xiph/flac/archive/refs/tags/${flac_version}.tar.gz" -o "${flac_pkg}"
+# fi
 
-echo "Building libFLAC..."
+# echo "Building libFLAC..."
 
-rm -rf ${flac_dir}
-tar xzf ${flac_pkg} \
-    && pushd ${flac_dir} >/dev/null 2>&1 \
-    && mkdir -p build \
-    && pushd build >/dev/null 2>&1 \
-    && cmake \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_C_COMPILER="${CC}" \
-    -DCMAKE_C_FLAGS="${CFLAGS}" \
-    -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
-    -DBUILD_DOCS=OFF \
-    -DWITH_OGG=OFF \
-    -DBUILD_CXXLIBS=OFF \
-    -DBUILD_PROGRAMS=OFF \
-    -DBUILD_UTILS=OFF \
-    -DBUILD_TESTING=OFF \
-    -DBUILD_EXAMPLES=OFF \
-    -DBUILD_SHARED_LIBS=ON \
-    -DINSTALL_MANPAGES=OFF \
-    -DENABLE_MULTITHREADING=ON \
-    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-    .. \
-    && make -j ${MAKEJ} install \
-    && popd >/dev/null 2>&1 \
-    && popd >/dev/null 2>&1
+# rm -rf ${flac_dir}
+# tar xzf ${flac_pkg} \
+#     && pushd ${flac_dir} >/dev/null 2>&1 \
+#     && mkdir -p build \
+#     && pushd build >/dev/null 2>&1 \
+#     && cmake \
+#     -DCMAKE_BUILD_TYPE=Release \
+#     -DCMAKE_C_COMPILER="${CC}" \
+#     -DCMAKE_C_FLAGS="${CFLAGS}" \
+#     -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+#     -DBUILD_DOCS=OFF \
+#     -DWITH_OGG=OFF \
+#     -DBUILD_CXXLIBS=OFF \
+#     -DBUILD_PROGRAMS=OFF \
+#     -DBUILD_UTILS=OFF \
+#     -DBUILD_TESTING=OFF \
+#     -DBUILD_EXAMPLES=OFF \
+#     -DBUILD_SHARED_LIBS=ON \
+#     -DINSTALL_MANPAGES=OFF \
+#     -DENABLE_MULTITHREADING=ON \
+#     -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+#     .. \
+#     && make -j ${MAKEJ} install \
+#     && popd >/dev/null 2>&1 \
+#     && popd >/dev/null 2>&1
 
 popd
